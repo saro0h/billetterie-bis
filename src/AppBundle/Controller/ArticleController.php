2d56bc316f01c5c2dfaff5cb22c47444bdaf1312
sarah khalil <mkhalil.sarah@gmail.com>
2016-02-03 17:21:01 +0100

    Finished CRUD for article.

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -6,14 +6,19 @@ use AppBundle\Entity\Article;
 use AppBundle\Type\ArticleType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfToken;
 
+/**
+ * @Route("/admin/article")
+ */
 class ArticleController extends Controller
 {
     /**
      * @Template
-     * @Route("/article/new", name="article_new")
+     * @Route("/new", name="article_new")
      */
     public function newAction(Request $request)
     {
@@ -32,13 +37,15 @@ class ArticleController extends Controller
             return $this->redirectToRoute('article_new');
         }
 
-        return array(
-            'articleForm' => $form->createView()
-        );
+        return array('articleForm' => $form->createView());
     }
 
     /**
-     * @Route("/article/{id}", name="article_update")
+     * @Route(
+     *     "/{id}",
+     *     name="article_update",
+     *     requirements={ "id" = "\d+"}
+     * )
      * @Template("AppBundle:Article:new.html.twig")
      */
     public function updateAction(Request $request, Article $article)
@@ -47,8 +54,7 @@ class ArticleController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
+            $this->getDoctrine()->getManager()->flush();
 
             $this->addFlash('success', 'The article has been successfully updated.');
 
@@ -58,8 +64,44 @@ class ArticleController extends Controller
             );
         }
 
-        return array(
-            'articleForm' => $form->createView()
-        );
+        return array('articleForm' => $form->createView());
+    }
+
+    /**
+     * @Route("/", name="article_list")
+     * @Template
+     */
+    public function listAction()
+    {
+        $articles = $this->getDoctrine()->getRepository('AppBundle:Article')->findAll();
+
+        return array('articles' => $articles);
+    }
+
+    /**
+     * @Route("/delete", name="article_delete")
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request)
+    {
+        if (!$article = $this->getDoctrine()->getRepository('AppBundle:Article')->findOneById($request->request->get('article_id'))) {
+            $this->addFlash('error', 'The article you want to delete does not exist.');
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        $csrf_token = new CsrfToken('delete_article', $request->request->get('_csrf_token'));
+
+        if ($this->get('security.csrf.token_manager')->isTokenValid($csrf_token)) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($article);
+            $em->flush();
+
+            $this->addFlash('success', 'You have successfully deleted the article.');
+        } else {
+            $this->addFlash('error', 'An error occurred.');
+        }
+
+        return $this->redirectToRoute('article_list');
     }
 }
