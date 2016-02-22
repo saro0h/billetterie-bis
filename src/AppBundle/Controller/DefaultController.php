a91dd4ce015bda2907a055dc19b6caffa3bf4e33
pavithra27sam <pavithra27sam@gmail.com>
2016-02-22 16:49:33 +0100

    Added new routing for concerts

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/post/{id}", name="post")
+     * @Route("/post/{slug}", name="post")
      */
     public function postAction(Article $article)
     {
@@ -32,5 +32,32 @@ class DefaultController extends Controller
         $articles = $this->getDoctrine()->getRepository('AppBundle:Article')->findAll();
 
         return $this->render('AppBundle:default:posts.html.twig', array('articles' => $articles));
+    }
+    
+    /**
+     * @Route("/buytickets", name="buytickets")
+     */
+    public function buyticketsAction()
+    {
+        $concerts = $this->getDoctrine()->getRepository('AppBundle:Concert')->findAll();
+
+        return $this->render('AppBundle:default:buytickets.html.twig', array('concerts' => $concerts));
+    }
+    
+    /**
+     * @Route("/buy", name="buy")
+     */
+    public function buyAction()
+    {
+        $message = \Swift_Message::newInstance()
+                ->setSubject('Confirmation mail')
+                ->setFrom('dummycompte2016@gmail.com')
+                ->setTo('dummycompte2016@gmail.com')
+                ->setBody('Your concert has been reserved successfully');
+               
+        $this->get('mailer')->send($message);
+        
+        return $this->render('AppBundle:default:buy.html.twig');
+     
     }
 }
